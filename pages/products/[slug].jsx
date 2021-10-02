93da43fab049b610273ace77f2c2eeb1c5fbc8a5
Joris Nijkamp <JGW.Nijkamp@student.han.nl>
2021-10-02 15:49:18 +0200

    Updated slug

--- pages/products/[slug].jsx
+++ pages/products/[slug].jsx
@@ -1,31 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
 
-import products from 'products.json';
+// import products from 'products.json';
 import {API_URL} from "~/utils/urls";
 import MediaOne from "~/components/partials/product/media/media-one";
 import DetailThree from "~/components/partials/product/detail/detail-three";
-import OwlCarousel from "~/components/features/owl-carousel";
-import {mainSlider17} from "~/utils/data/carousel";
 
 
 function ProductDefault({product}) {
-    // const { data, loading, error } = useQuery( GET_PRODUCT, { variables: { slug } } );
-    const [ loaded, setLoadingState ] = useState( false );
-    // const product = data && data.product.data;
-    // const related = data && data.product.related;
-
-    console.log(product)
-
-    useEffect( () => {
-        if ( !product )
-            imagesLoaded( 'main' ).on( 'done', function () {
-                setLoadingState( true );
-            } ).on( 'progress', function () {
-                setLoadingState( false );
-            } );
-    }, [ product ] )
-
     return (
         <main className="main mt-4 border-top single-product">
             <Helmet>
@@ -35,46 +17,23 @@ function ProductDefault({product}) {
 
             <h1 className="d-none">{product.name}</h1>
             {
-                products !== undefined ?
-                    <div className={`page-content mb-10`}>
-                        <div className="container skeleton-body">
-                            <div className="products products-single row mb-4">
-                                <div className="col-md-6">
-                                    <MediaOne product={product}/>
-                                </div>
-
-                                <div className="col-md-6">
-                                    <DetailThree product={product} isSticky={true} isDesc={false}/>
-                                </div>
-                            </div>
-
-                            {/*<DescOne products={ products } isShipping={ true } isGuide={ false } />*/}
-
-                            {/*<RelatedProducts products={ related } adClass="products-wrapper" />*/}
-                        </div>
-                    </div> : <div className="skeleton-body container mb-10">
-                        <div className="row mb-7">
-                            <div className="col-md-6 pg-vertical">
-                                <div className="skel-pro-gallery"></div>
+                <div className={`page-content mb-10`}>
+                    <div className="container skeleton-body">
+                        <div className="products products-single row mb-4">
+                            <div className="col-md-6">
+                                <MediaOne product={product}/>
                             </div>
 
                             <div className="col-md-6">
-                                <div className="skel-pro-summary"></div>
+                                <DetailThree product={product} isSticky={true} isDesc={false}/>
                             </div>
                         </div>
-                        <div className="skel-pro-tabs"></div>
-                        <section className="pt-3 mt-4">
-                            <h2 className="title justify-content-center">Related Products</h2>
 
-                            <OwlCarousel adClass="owl-carousel owl-theme owl-nav-full" options={ mainSlider17 }>
-                                {
-                                    [ 1, 2, 3, 4, 5, 6 ].map( ( item ) =>
-                                        <div className="product-loading-overlay" key={ 'popup-skel-' + item }></div>
-                                    )
-                                }
-                            </OwlCarousel>
-                        </section>
+                        {/*<DescOne products={ products } isShipping={ true } isGuide={ false } />*/}
+
+                        {/*<RelatedProducts products={ related } adClass="products-wrapper" />*/}
                     </div>
+                </div>
             }
         </main>
     )
