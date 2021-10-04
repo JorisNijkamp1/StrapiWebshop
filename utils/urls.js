d15f64e8cc8d8d091a140b1e90ff5322ce474974
Joris Nijkamp <JGW.Nijkamp@student.han.nl>
2021-10-04 21:04:33 +0200

    Urls

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_KEY;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://strapi-webshop.jorisnijkamp.nl'
+export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_KEY || 'pk_live_5674B79447EDA8CE';
 
 export const fromImageToUrl = image => {
     if (!image) {
